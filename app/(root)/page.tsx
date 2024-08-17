1582bf5f0f3f5ed738cf2e3624035e68990d2653
jwu02 <jiacongwu31@gmail.com>
2024-08-17 20:48:26 +0100

    Update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,14 @@
 import ActivityDashboard from "@/components/activity/ActivityDashboard"
 import Banner from "@/components/Banner"
 import KnowledgeGraph from "@/components/knowledge-base/KnowledgeBaseDashboard"
-import { fetchKnowledgeGraphData } from "@/lib/actions/knowledge.actions"
 
 export const revalidate = 24*60*60 // revalidate every x seconds
 
 export default async function Home() {
-  const { nodes, links } = await fetchKnowledgeGraphData()
-
   return (
     <>
       <Banner />
-      <KnowledgeGraph nodes={nodes} links={links} />
+      <KnowledgeGraph />
       <ActivityDashboard />
     </>
   )
--- components/knowledge-base/KnowledgeBaseDashboard.tsx
+++ components/knowledge-base/KnowledgeBaseDashboard.tsx
@@ -1,6 +1,8 @@
+import { fetchKnowledgeGraphData } from '@/lib/actions/knowledge.actions'
 import DisjointForceGraph from '@/components/knowledge-base/DisjointForceGraph'
 
-const KnowledgeGraph = ({ nodes, links }) => {
+const KnowledgeGraph = async () => {
+  const { nodes, links } = await fetchKnowledgeGraphData()
 
   return (
     <div className="flex flex-col gap-5 w-full">
