8f282a4646fff482bf04b38d1d0ed66f0af9ed34
jwu02 <jiacongwu31@gmail.com>
2025-03-23 22:53:00 +0000

    supposedly revalidates every hour

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,17 @@
 import { getBlogs } from '@/lib/actions/notion.actions'
 import Blogs from '@/components/blog/Blogs'
+import { unstable_cache } from 'next/cache'
+
+const getCachedBlogs = unstable_cache(
+  async () => {
+    const blogs = await getBlogs()
+    return blogs.filter((blog): blog is NonNullable<typeof blog> => blog !== null)
+  },
+  ['blogs'],
+  { revalidate: 3600 } // Revalidate every hour
+)
 
 export default async function BlogPage() {
-  const blogs = await getBlogs()
-  const filteredBlogs = blogs.filter((blog): blog is NonNullable<typeof blog> => blog !== null)
-
-  return <Blogs blogs={filteredBlogs} />
+  const blogs = await getCachedBlogs()
+  return <Blogs blogs={blogs} />
 }
